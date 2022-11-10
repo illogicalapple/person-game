205abe4379c849faf63971211ed50cb1d454231c
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2022-11-09 18:07:45 -0600

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const $ = e => document.querySelector(e);
+var game = {
+  set text(newText) {
+    this._text = newText;
+    this._updateText();
+  },
+  get text() {
+    return this._text;
+  },
+  _updateText: function() {
+    $("pre.game").innerHTML = md.render(this._text);
+  },
+  _text = "[if you see this something has gone horribly wrong]"
+};
+$("span.buttons button").addEventListener("click", () => game.text = `**abc**  
+lorem ipsum dolor sit amet
+*test*`)
