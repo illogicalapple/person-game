701201cc38f285c4141c72d5bb4121ac4977877c
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2022-11-09 19:13:18 -0600

    few fixes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const $ = e => document.querySelector(e);
+const md = markdownit();
 var game = {
   set text(newText) {
     this._text = newText;
@@ -12,10 +13,11 @@ var game = {
   },
   _text = "[if you see this something has gone horribly wrong]"
 };
+$.all = e => document.querySelectorAll(e);
 
 // dom things
 addEventListener("load", () => {
-  $("span.buttons button").addEventListener("click", () => game.text = `**abc**  
+  $.all("span.buttons button").forEach(e => e.addEventListener("click", () => game.text = `**abc**  
 lorem ipsum dolor sit amet
-*test*`);
+*test*`));
 });
